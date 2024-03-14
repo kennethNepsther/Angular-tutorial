5ae54ad65456123426ee6e2d836ebc32a262546d
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-03-14 22:03:54 +0100

    Class, properties, methods and object

--- src/app/portfolio/student/student.component.ts
+++ src/app/portfolio/student/student.component.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
-export class StudentComponent {
+export class StudentComponent {  // class
+
+  // Properties
+   /* private name:String;
+   private age:number;
+   private email:String; */
+
+
+    name:String;
+    age:number;
+    email:String;
+
+
+
+// Constructor
+  constructor(){
+    this.name = 'kenneth';
+    this.age = 21;
+    this.email = 'kenneth@gmail.com';
+
+  }
+
+  // Method
+  greet(){
+
+    console.log("chamando a função greeter");
+
+  }
+
 
 }
+
+// Acessing properties
+const estudent = new StudentComponent();
+console.log(estudent.name); // output: kenneth
+console.log(estudent.age);
+console.log(estudent.email);
+
+estudent.greet();
+
+
