a374b75f9c1ef9e735cb9d13f58d6ae4041c89e0
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-03-16 00:48:32 +0100

    data binding

--- src/app/portfolio/student/student.component.ts
+++ src/app/portfolio/student/student.component.ts
@@ -1,25 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
-export class StudentComponent {  // class
+export class StudentComponent {
+// for reactive forms
+userForm:any;
+isFormVisible:boolean = true;
+userRole:string ='admin';
+items:string[]=['item 1','item 2','item 3'];
+
+constructor(public formbuilder:FormBuilder){
+
+
+}
+
+ngOnInit(){
+  this.userForm = this.formbuilder.group({
+    name:['',Validators.required],
+    email:['']
+
+  })
+
+
+}
+
+onSubmit(){
+  console.log("User Details",this.userForm.value);
+}
+
+
+
+}
+
+
+
+
+
+
+
+/* export class StudentComponent {  // class
 
   // Properties
-   /* private name:String;
-   private age:number;
-   private email:String; */
-
-
     name:String;
     age:number;
     email:String;
-
-
 
 // Constructor
   constructor(){
@@ -32,8 +63,7 @@ export class StudentComponent {  // class
   // Method
   greet(){
 
-    //console.log("chamando a função greeter");
-
+    console.log("chamando a função greeter");
   }
 
 
@@ -43,10 +73,11 @@ export class StudentComponent {  // class
 const estudent = new StudentComponent();
 
 // Acessing properties
-//console.log(estudent.name); // output: kenneth
-//console.log(estudent.age);
-//console.log(estudent.email);
+console.log(estudent.name); // output: kenneth
+console.log(estudent.age);
+console.log(estudent.email);
 
 estudent.greet();
+ */
 
 
